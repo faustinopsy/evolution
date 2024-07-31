87d011461313ce1606555239ad446916f73272cd
faustinopsy <rodrigohipnose@gmail.com>
2024-07-31 16:41:03 -0300

    separando a navbar

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const navBar = document.querySelector('nav')
+    navBar.innerHTML = `<a href="index.html">Home</a>
+                        <a href="contato.html">Contato</a>`;
+    
+})
