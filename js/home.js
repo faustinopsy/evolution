28b0ced760867ae2042f1710582d2718153e8529
faustinopsy <rodrigohipnose@gmail.com>
2024-07-31 18:23:11 -0300

    modified:   js/home.js

--- js/home.js
+++ js/home.js
@@ -6,8 +6,8 @@ export class Home {
     carregar() {
         return `
             <div style="background-color: rgb(135, 218, 20);">
-            <h1>Página inicial!</h1>
-        </div>
+                <h1>Página inicial!</h1>
+            </div>
         `;
     }
 }
